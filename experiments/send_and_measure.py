ba619e6b222eedc08ee4f88ba3133111c6a64f88
gitdylanj <ddj123@uw.edu>
2026-05-31 19:43:50 -0700

    updated send and measure for arb waveforms

--- experiments/send_and_measure.py
+++ experiments/send_and_measure.py
@@ -4,6 +4,7 @@ from pyflux.core.experiment import ExperimentalContext
 from pyflux.core.block import Signal, ActionBlock, FunctionalBlock, ResamplingBlock
 from pyflux.core.chain import Chain
 import matplotlib.pyplot as plt
+import numpy as np
 import time
 
 
@@ -24,44 +25,49 @@ class SendSine(ActionBlock):
             frequency=self.freq,
             amplitude="1.0",
             offset="0.0")
+        
 
+class SendArbWaveform(ActionBlock):
+    def __init__(self, fs, fn_generator_driver, waveform_data: np.ndarray, freq: float,
+                 amplitude: float, offset: float):
+        super().__init__(fs)
+        self.driver = fn_generator_driver
+        self.waveform_data = waveform_data
+        self.freq = freq
+        self.amplitude = amplitude
+        self.offset = offset
+        assert len(waveform_data) <= 16_384, "Agilent 33250A max arbitrary waveform length is 16384 points"
+    
+    def action(self, x: Signal):
+        self.driver.send_arbitrary(
+            samples=self.waveform_data,
+            freq=self.freq,
+            amplitude=self.amplitude,
+            offset=self.offset)
+        
 class MeasureSine(ActionBlock):
-    def __init__(self, fs, osc_driver, input_signal_frequency):
+    def __init__(self, fs, osc_driver, input_signal_frequency, channel):
         super().__init__(fs)
         self.driver = osc_driver
         self.input_signal_frequency = input_signal_frequency
+        self.channel = channel
 
     def action(self, x: Signal) -> Signal:
-        self.driver.set_trigger(ch=1, voltage_level=0.5)
-        self.driver.configure_channel(ch=3,
-                                      scale=1, offset=0,
-                                      seconds_per_div=1/self.input_signal_frequency)
-        x.data, _ = self.driver.measure_waveform(channel=3)
+        _, x.data = self.driver.measure_waveform(channel=self.channel)
         x.sampling_rate = self.driver.input_sample_rate
 
-class DisplaySine(FunctionalBlock):
+class DisplayWaveform(FunctionalBlock):
     def __init__(self, fs):
         super().__init__(fs)
 
     def transform(self, x):
-        # Exp.log(f"{x.data, x.data.max().item(), x.data.min().item()}")
         plt.plot(x.data)
-        plt.title("Measured Sine Waveform")
+        plt.title("Measured Waveform")
         plt.xlabel("Sample Number")
-        plt.ylabel("Amplitude (ADC Units)")
+        plt.ylabel("Amplitude (Volts)")
         plt.grid()
         plt.show()
         return x
-
-class PrintSomeNums(ActionBlock):
-    def __init__(self, fs, mode: str, name=None):
-        super().__init__(fs, mode, name)
-    
-    def action(self, x):
-        pid = os.getpid()
-        for i in range(3):
-            time.sleep(4)
-            print(f"{self.mode} {i} on PID: {pid}")
 
 class ResampleWaveform(ResamplingBlock):
     def __init__(self, fs_in, fs_out):
@@ -79,27 +85,46 @@ if __name__ == '__main__':
     '''Without the main guard, every child process will rerun the code'''
     with ExperimentalContext(CONFIG_FILE="send_and_measure.yml") as Exp:
 
-        Exp.log("Starting Sine sending!")
+        FREQS = [1e4, 2e4, 5e4] # Hz
 
         fn_gen = Exp.Agilent_33250A
         osc = Exp.Tektronix_TDS2000
+    
+        osc.set_record_length(20_000)
+        osc.set_trigger(ch=1, voltage_level=1)
 
-        main_pipe = Chain(chain=[
-            SendSine(fs=fn_gen.output_sample_rate, freq=1e4, fn_generator_driver=fn_gen), # 10 kHz
-            ResampleWaveform(fs_in=fn_gen.output_sample_rate, fs_out=osc.input_sample_rate),
-            MeasureSine(fs=osc.input_sample_rate, osc_driver=osc, input_signal_frequency=1e4),
-            DisplaySine(fs=osc.input_sample_rate)
-        ])
+    
+        osc.set_probe_gain(ch=3, gain=1)
+        osc.set_probe_gain(ch=1, gain=1)
 
-        async_chain = Chain(chain=[
-            PrintSomeNums(fs=None, mode="thread"),
-            PrintSomeNums(fs=None, mode="process")
-        ])
+        VERTICAL_DIVS = osc.vertical_divisions
+        SINE_VPKPK = 2
 
-        for i in range(Exp.config.SINE_ITERS):
+        osc.configure_channel(ch=3,
+                            scale=SINE_VPKPK/VERTICAL_DIVS, offset=0)
+        osc.configure_channel(ch=1,
+                            scale=1, offset=0)
+
+        for f in FREQS:
+
+            Exp.log(f"Starting Sine sending at {f}!")
+
+            osc.set_horizontal_scale(seconds_per_div=0.1*(1/f))
+            N = 16384 # max arbitrary waveform length for Agilent 33250A
+            t = np.linspace(0, 1, N, endpoint=False)
+            waveform_data = 1 - 2 * np.abs(2 * t - 1)
+
+            
+            main_pipe = Chain(chain=[
+                SendArbWaveform(fs=fn_gen.output_sample_rate,
+                              freq=f,
+                              fn_generator_driver=fn_gen,
+                              waveform_data=waveform_data,
+                              amplitude=1.0,
+                              offset=0.0
+                              ), # 10 kHz
+                ResampleWaveform(fs_in=fn_gen.output_sample_rate, fs_out=osc.input_sample_rate),
+                MeasureSine(fs=osc.input_sample_rate, osc_driver=osc, input_signal_frequency=f, channel=3)
+            ])
             x = Signal(data=None, sampling_rate=fn_gen.output_sample_rate)
-            futures = async_chain.run_async(x, exp=Exp)
             main_pipe.run(x) 
-
-        Exp.log("Hardware finished! Finishing processing tasks. . ")
-        Exp.sync(futures)
